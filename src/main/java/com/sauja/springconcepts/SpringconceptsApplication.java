0989645683c9c218b92717b3742359f08ca65506
Saurabh Jadhav <sauja7@gmail.com>
2017-11-15 17:35:43 -0500

    init project

---
+++ src/main/java/com/sauja/springconcepts/SpringconceptsApplication.java
@@ -0,0 +1,12 @@
+package com.sauja.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconceptsApplication.class, args);
+	}
+}
